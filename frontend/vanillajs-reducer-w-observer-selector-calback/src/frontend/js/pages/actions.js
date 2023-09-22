39fc907b27e23c09403deedcd82761452862cdb3
paulAlexSerban <paul.alex.serban@gmail.com>
2023-09-22 16:44:32 +0300

    updates

--- frontend/vanillajs-reducer-w-observer-selector-calback/src/frontend/js/pages/actions.js
+++ frontend/vanillajs-reducer-w-observer-selector-calback/src/frontend/js/pages/actions.js
@@ -1,15 +1,4 @@
 import { SET_ACTIVE_ITEM, TOGGLE_SUBMENU } from './actionTypes';
 
-export const setActiveItemAction = (payload) => {
-    if (!payload || typeof payload !== 'string') {
-        throw new Error('Invalid payload for SET_ACTIVE_ITEM');
-    }
-    return { type: SET_ACTIVE_ITEM, payload };
-};
-
-export const toggleSubmenuAction = (payload) => {
-    if (!payload || typeof payload !== 'string') {
-        throw new Error('Invalid payload for TOGGLE_SUBMENU');
-    }
-    return { type: TOGGLE_SUBMENU, payload };
-};
+export const setActiveItemAction = (payload) => ({ type: SET_ACTIVE_ITEM, payload });
+export const toggleSubmenuAction = (payload) => ({ type: TOGGLE_SUBMENU, payload });
